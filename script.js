4457b211997546bc77a9a51e8f9d602745c9a1f9
ren <160462399+ktmagno@users.noreply.github.com>
2024-05-06 21:15:59 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,43 +2,152 @@ $(document).ready(function () {
   $("#minus1, #minus2, #minus3, #minus4").hide();
   $("#p1, #p2, #p3, #p4").hide();
 
-  //question 1
-  $("#minus1").click(function () {
-    $("#p1, #minus1").hide();
-    $("#plus1").show();
+  document.getElementsByTagName("h2")[0].focus();
+
+  //MOUSE NAV
+
+  //QUESTION 1
+  $("#q1").click(function () {
+    $("#p1").toggle(function () {
+      if ($("#p1").is(":hidden") == false) {
+        $("#minus1").show();
+        $("#plus1").hide();
+      } else {
+        $("#minus1").hide();
+        $("#plus1").show();
+      }
+    });
   });
-  $("#q1, #plus1").click(function () {
+
+  $("#plus1").click(function () {
     $("#p1, #minus1").show();
     $("#plus1").hide();
   });
 
-  //   question 2
-  $("#minus2").click(function () {
-    $("#p2, #minus2").hide();
-    $("#plus2").show();
+  $("#minus1").click(function () {
+    $("#p1, #minus1").hide();
+    $("#plus1").show();
   });
-  $("#q2, #plus2").click(function () {
+
+  //QUESTION 2
+  $("#q2").click(function () {
+    $("#p2").toggle(function () {
+      if ($("#p2").is(":hidden") == false) {
+        $("#minus2").show();
+        $("#plus2").hide();
+      } else {
+        $("#minus2").hide();
+        $("#plus2").show();
+      }
+    });
+  });
+
+  $("#plus2").click(function () {
     $("#p2, #minus2").show();
     $("#plus2").hide();
   });
 
-  //question 3
-  $("#minus3").click(function () {
-    $("#p3, #minus3").hide();
-    $("#plus3").show();
+  $("#minus2").click(function () {
+    $("#p2, #minus2").hide();
+    $("#plus2").show();
   });
-  $("#q3, #plus3").click(function () {
+
+  //QUESTION 3
+  $("#q3").click(function () {
+    $("#p3").toggle(function () {
+      if ($("#p3").is(":hidden") == false) {
+        $("#minus3").show();
+        $("#plus3").hide();
+      } else {
+        $("#minus3").hide();
+        $("#plus3").show();
+      }
+    });
+  });
+
+  $("#plus3").click(function () {
     $("#p3, #minus3").show();
     $("#plus3").hide();
   });
 
-  //question 4
+  $("#minus3").click(function () {
+    $("#p3, #minus3").hide();
+    $("#plus3").show();
+  });
+
+  //QUESTION 4
+  $("#q4").click(function () {
+    $("#p4").toggle(function () {
+      if ($("#p4").is(":hidden") == false) {
+        $("#minus4").show();
+        $("#plus4").hide();
+      } else {
+        $("#minus4").hide();
+        $("#plus4").show();
+      }
+    });
+  });
+
+  $("#plus4").click(function () {
+    $("#p4, #minus4").show();
+    $("#plus4").hide();
+  });
   $("#minus4").click(function () {
     $("#p4, #minus4").hide();
     $("#plus4").show();
   });
-  $("#q4, #plus4").click(function () {
-    $("#p4, #minus4").show();
-    $("#plus4").hide();
+
+  //KEYBOARD NAV
+
+  //QUESTION 1
+  $("#q1").keypress(function () {
+    $("#p1").toggle(function () {
+      if ($("#p1").is(":hidden") == false) {
+        $("#minus1").show();
+        $("#plus1").hide();
+      } else {
+        $("#minus1").hide();
+        $("#plus1").show();
+      }
+    });
+  });
+
+  //QUESTION 2
+  $("#q2").keypress(function () {
+    $("#p2").toggle(function () {
+      if ($("#p2").is(":hidden") == false) {
+        $("#minus2").show();
+        $("#plus2").hide();
+      } else {
+        $("#minus2").hide();
+        $("#plus2").show();
+      }
+    });
+  });
+
+  //QUESTION 3
+  $("#q3").keypress(function () {
+    $("#p3").toggle(function () {
+      if ($("#p3").is(":hidden") == false) {
+        $("#minus3").show();
+        $("#plus3").hide();
+      } else {
+        $("#minus3").hide();
+        $("#plus3").show();
+      }
+    });
+  });
+
+  //QUESTION 4
+  $("#q4").keypress(function () {
+    $("#p4").toggle(function () {
+      if ($("#p4").is(":hidden") == false) {
+        $("#minus4").show();
+        $("#plus4").hide();
+      } else {
+        $("#minus4").hide();
+        $("#plus4").show();
+      }
+    });
   });
 });
